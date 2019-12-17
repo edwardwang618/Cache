f54fb2dba97ddd739d54a144cf8a61baa3fba609
edw_glb <edward.wang618@gmail.com>
2019-12-17 02:46:27 -0600

    Use lombok annotation allargsconstructor

--- spring-caching/src/main/java/com/example/caching/caching/Book.java
+++ spring-caching/src/main/java/com/example/caching/caching/Book.java
@@ -1,15 +1,12 @@
 package com.example.caching.caching;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Book {
     private String isbn;
     private String title;
-    
-    public Book(String isbn, String title) {
-        this.isbn = isbn;
-        this.title = title;
-    }
     
 }
